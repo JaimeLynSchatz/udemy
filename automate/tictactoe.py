a0c3b81694d8e52efa8fc97019532ac15668d6b9
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2017-03-25 21:19:57 -0700

    beginnings of tictactoe game

---
+++ automate/tictactoe.py
@@ -0,0 +1,22 @@
+import pprint
+
+cat = {'name': 'Zophie', 'age': 7, 'color': 'gray'}
+allCats = []
+allCats.append({'name': 'Zopie', 'age': 7, 'color': 'gray'})
+allCats.append({'name': 'Garfield', 'age': 10, 'color': 'orange'})
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+			'mid-L': ' ', 'mid-M': 'X', 'mid-R': ' ',
+			'low-L': ' ', 'low-M': ' ', 'low-R': ' ', }
+
+pprint.pprint(theBoard)
+
+def printBoard(board):
+	print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+	print('-----')
+	print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+	print('-----')
+	print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+printBoard(theBoard)
+
