b6762db48a301678890c3fbcdd79667932086b6b
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2017-05-15 20:18:08 -0700

    automation course - last bits

---
+++ automate/keyboards.py
@@ -0,0 +1,14 @@
+# keyboard control
+
+pyautogui.click(2100, 400) # for when text editor is in upper right hand corner
+pyautogui.typewrite('Hello world!')
+
+pyautogui.click(2100, 600)
+pyautogui.typewrite(['a', 'b', 'c', 'left', 'left', 'left', 'X', 'Y'])
+
+pyautogui.hotkey('ctrl', 'o')
+pyautogui.press('shift')
+
+pyautogui.keyboard.keys
+
+# Hello world!Hello world!Hello world!Hello world!Hello world!
